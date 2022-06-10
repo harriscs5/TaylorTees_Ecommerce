7d0897c3a0a76f3a3c9db25058e0d9203e3671e4
Nelly <$>
2022-06-10 16:27:02 -0400

    installed react router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,10 +14,15 @@ function App() {
             <div className="products">{
               data.products.map(product => (
                 <div className="product" key={product.slug}>
+                <a href={`/product/${product.slug}`}>
                   <img src={product.image} alt={product.name}/>
+                </a>
                   <div className="product-info">
+                  <a href={`/product/${product.slug}`}>
                     <p>{product.name}</p>
-                    <p> ${product.price}</p>
+                  </a>
+                    <p> <strong>${product.price}</strong></p>
+                    <button>Add to Cart</button>
                   </div>
 
               </div>
