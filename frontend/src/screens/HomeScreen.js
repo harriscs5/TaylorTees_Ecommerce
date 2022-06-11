5a5d80e6a624861a2e9a14d82cc87680633a329c
Nelly <$>
2022-06-11 16:53:30 -0400

    created reducer function, try catch statement to catch errors, and imported useReducer to homescreen

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,10 +1,10 @@
-import { useEffect, useState} from 'react';
+import { useEffect, useReducer, useState} from 'react';
 import { Link } from "react-router-dom";
 //import data from "../data";
 import axios from 'axios';
 
 const reducer = (state, action) => {
-  switch(action.type) {
+  switch (action.type) {
     case 'FETCH_REQUEST':
       return {...state, loading: true}; //shows loading box
     case 'FETCH_SUCCESS':
@@ -18,14 +18,22 @@ const reducer = (state, action) => {
 
 function HomeScreen() {
   const [{loading, error, products}, dispatch] = useReducer(reducer, {
-    loading: true, error: '',
+    products: [],
+    loading: true,
+    error: '',
   });
   //const [products, setProducts] = useState([]);
   useEffect(() => {
     const fetchData = async () => {
       dispatch({type: 'FETCH_REQUEST'});
-      const result = await axios.get('/api/products');
-      setProducts(result.data);
+      try {
+        const result = await axios.get('/api/products');
+        dispatch({type: 'FETCH_SUCCESS', payload: result.data});
+      } catch (error) {
+        dispatch({type: 'FETCH_FAIL', payload: error.message});
+      }
+     
+      //setProducts(result.data);
     };
     fetchData();
 }, []);
