2d1d115cd9121478c05f4ebc7518828d659508bd
Nelly <$>
2022-06-10 22:33:04 -0400

    chaning achor tags to link tags for react router

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -7,13 +7,13 @@ function HomeScreen() {
             <div className="products">{
               data.products.map(product => (
                 <div className="product" key={product.slug}>
-                <Link href={`/product/${product.slug}`}>
+                <Link to={`/product/${product.slug}`}>
                   <img src={product.image} alt={product.name}/>
-                </link>
+                </Link>
                   <div className="product-info">
-                  <a href={`/product/${product.slug}`}>
+                  <Link to={`/product/${product.slug}`}>
                     <p>{product.name}</p>
-                  </a>
+                  </Link>
                     <p> <strong>${product.price}</strong></p>
                     <button>Add to Cart</button>
                   </div>
