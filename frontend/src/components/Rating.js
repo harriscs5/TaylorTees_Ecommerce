0f53a6f9a7e039086359b604bfaaf01efc98f7eb
Nelly <$>
2022-06-11 23:07:47 -0400

    wrote code to display 5 star rating

--- frontend/src/components/Rating.js
+++ frontend/src/components/Rating.js
@@ -2,11 +2,51 @@ function Rating(props) {
     const {rating, numreReviews} = props;
     return (
         <div className="rating">
-            <span>
+            <span> 
                 <i className={
-                    rating>=1
+                    rating >= 1
                     ? 'fas fa-star'
                     : rating>=0.5 
+                    ? 'fa-star-half-alt'
+                    : 'far fa-star'
+                } 
+            />
+            </span>
+            <span>
+                <i className={
+                    rating >= 2
+                    ? 'fas fa-star'
+                    : rating>=1.5 
+                    ? 'fa-star-half-alt'
+                    : 'far fa-star'
+                } 
+            />
+            </span>
+            <span>
+                <i className={
+                    rating >= 3
+                    ? 'fas fa-star'
+                    : rating >= 2.5 
+                    ? 'fa-star-half-alt'
+                    : 'far fa-star'
+                } 
+            />
+            </span>
+            <span>
+                <i className={
+                    rating >= 4
+                    ? 'fas fa-star'
+                    : rating>=3.5 
+                    ? 'fa-star-half-alt'
+                    : 'far fa-star'
+                } 
+            />
+            </span>
+            <span>
+                <i className={
+                    rating >= 5
+                    ? 'fas fa-star'
+                    : rating>=4.5 
                     ? 'fa-star-half-alt'
                     : 'far fa-star'
                 } 
