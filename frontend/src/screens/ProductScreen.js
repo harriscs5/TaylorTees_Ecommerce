79727ec039400ab03b3853d5a3df5b73d531101f
Nelly <$>
2022-06-12 00:48:51 -0400

    created backed api for displaying info on product detail screen based on product slug

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,4 +1,6 @@
 import { useParams } from "react-router-dom";
+import { useEffect, useReducer, useState} from 'react';
+import axios from "axios";
 
 const reducer = (state, action) => {
     switch (action.type) {
@@ -27,7 +29,7 @@ function ProductScreen(){
         const fetchData = async () => {
           dispatch({type:'FETCH_REQUEST'});
           try {
-            const result = await axios.get('/api/products');
+            const result = await axios.get(`/api/products/slug/${slug}`);
             dispatch({type:'FETCH_SUCCESS', payload: result.data});
           } catch (err) {
             dispatch({type:'FETCH_FAIL', payload: err.message});
@@ -36,7 +38,7 @@ function ProductScreen(){
           //setProducts(result.data);
         };
         fetchData();
-    }, []);
+    }, [slug]);
 
     return (
         <h1>{slug}</h1>
