782b16ae59912ca1a38a7e312ec8d49a15d083b6
Nelly <$>
2022-06-12 00:38:47 -0400

    added code to fetch data from backend using usereducer and useeffect

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,8 +1,42 @@
 import { useParams } from "react-router-dom";
 
+const reducer = (state, action) => {
+    switch (action.type) {
+      case 'FETCH_REQUEST':
+        return {...state, loading: true}; //shows loading box
+      case 'FETCH_SUCCESS':
+        return {...state, products: action.payload, loading: false};
+      case 'FETCH_FAIL':
+        return {...state, loading: false, error: action.payload};
+      default:
+        return state; //if none of the 3 cases returns current state
+    }
+  };
+
 function ProductScreen(){
     const params = useParams();
     const {slug} = params;
+
+    const [{loading, error, product}, dispatch] = useReducer((reducer), {
+        products: [],
+        loading: true,
+        error: '',
+      });
+      //const [products, setProducts] = useState([]);
+      useEffect(() => {
+        const fetchData = async () => {
+          dispatch({type:'FETCH_REQUEST'});
+          try {
+            const result = await axios.get('/api/products');
+            dispatch({type:'FETCH_SUCCESS', payload: result.data});
+          } catch (err) {
+            dispatch({type:'FETCH_FAIL', payload: err.message});
+          }
+         
+          //setProducts(result.data);
+        };
+        fetchData();
+    }, []);
 
     return (
         <h1>{slug}</h1>
