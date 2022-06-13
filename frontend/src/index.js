200be1fce808bffc81f4c5c9010b761822eb509c
Nelly <$>
2022-06-13 16:47:27 -0400

    installed helmet provider

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <HelmetProvider>
-    <App />
+        <App />
     </HelmetProvider>
   </React.StrictMode>
 );
